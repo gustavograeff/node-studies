70e1e570bc13cee40cb4d4695a469af269ba5bf9
Gustavo <gustavograeff1998@gmail.com>
2020-03-18 01:34:20 -0300

    add: first file writer

--- js-files/routing-requests.js
+++ js-files/routing-requests.js
@@ -1,9 +1,11 @@
 
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  
+  const method = req.method;
+
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Second Node Page</title></head>');
@@ -12,6 +14,13 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
 
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'GRAEFF');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>')
