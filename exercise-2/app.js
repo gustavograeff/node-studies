922e81a10c2c118730ce075e2ffd8503974999c8
Gustavo <gustavograeff1998@gmail.com>
2020-04-05 22:46:50 -0300

    add: escersice 2

---
+++ exercise-2/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/users', (req, res, next) => {
+  console.log('second');
+  res.send('<h1>Response for /users</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('first');
+  res.send('<h1>Simple /</h1>');
+});
+
+app.listen(3000);
