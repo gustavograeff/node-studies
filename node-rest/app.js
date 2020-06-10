0e111740e8e5503b4cb6cd540a8919467da02a73
Gustavo <gustavograeff1998@gmail.com>
2020-06-10 13:41:12 -0300

    add: first rest api

--- node-rest/app.js
+++ node-rest/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const feedRoutes = require('./routes/feedRoutes');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  res.send({ name: 'Gustavo' });
+app.use(bodyParser.json());
+
+app.use((res, rex, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Acces-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
+
+app.use('/feed', feedRoutes);
 
 app.listen(8080);
